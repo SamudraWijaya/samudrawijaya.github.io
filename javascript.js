4890fc5c7b0436919c1f8cfbd6487d1120a6b7d7
SamudraWijaya <ossasam919@gmail.com>
2022-08-15 17:02:23 +0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,38 @@
+
+let nav = document.querySelector("nav");
+window.addEventListener("scroll", ()=>{
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+    }else{
+        nav.classList.remove("sticky")
+    }
+});
+{
+    const menuToogle = document.querySelector('.menu-toogle input');
+    const nav = document.querySelector('nav ul');
+    
+    menuToogle.addEventListener('click', function(){
+        nav.classList.toggle('slide')
+    });
+}
+
+{
+    const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction(){
+    if (window.pageYOffset > 300) {
+        backToTopButton.style.display = "block";
+    }
+    else{
+        backToTopButton.style.display = "none";
+    }
+}
+backToTopButton.addEventListener("click", backToTop);
+
+function backToTop(){
+    window.scrollTo(0, 0);
+}
+}
+
