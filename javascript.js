0ebb32f5e4dac2ece56d5bc36d4b9c9a1396b6dc
SamudraWijaya <110963396+SamudraWijaya@users.noreply.github.com>
2023-01-02 08:53:55 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
+// navbar
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector("nav ul");
+
+menu.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+});
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+//  sticky
 let nav = document.querySelector("nav");
 window.addEventListener("scroll", () => {
   if (document.documentElement.scrollTop > 90) {
@@ -6,31 +17,3 @@ window.addEventListener("scroll", () => {
     nav.classList.remove("sticky");
   }
 });
-{
-  const menuToogle = document.querySelector(".menu-toogle input");
-  const nav = document.querySelector("nav ul");
-
-  menuToogle.addEventListener("click", function () {
-    nav.classList.toggle("slide");
-  });
-}
-
-{
-  var backToTopButton = document.querySelector("#back-to-top-btn");
-
-  window.addEventListener("scroll", scrollFunction);
-
-  function scrollFunction() {
-    if (window.pageYOffset > 300) {
-      backToTopButton.style.display = "block";
-    } else {
-      backToTopButton.style.display = "none";
-    }
-  }
-  backToTopButton.addEventListener("click", backToTop);
-
-  function backToTop() {
-    window.scrollTo(0, 0);
-  }
-}
-
